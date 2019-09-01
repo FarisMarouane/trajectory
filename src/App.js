b7acc6da33b4f8cc03787adfcb97cfbd7697be2d
Marouane Faris <faris.marouane@gmail.com>
2019-09-01 19:46:21 +0200

    Use useState and useEffect hooks

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 import '../node_modules/react-vis/dist/style.css';
 import './App.css';
@@ -9,30 +9,44 @@ import User from './user';
 import { fromIndexToUserId } from './helpers';
 import UserDetail from './userDetail';
 
-class App extends React.PureComponent {
-  state = {
-    rawData: [],
-    dataToDisplay: {},
-    index: 0,
-    userId: undefined,
-    userPosition: { x: undefined, y: undefined },
-    showAll: false,
-    hoveredOverUserId: undefined,
-    displayDefaultTitle: true,
-    showUserStats: false,
-  };
+function App() {
+  const [index, setIndex] = useState(0);
+  const [userId, setUserId] = useState(undefined);
+  const [showAll, setShowAll] = useState(false);
+  const [displayDefaultTitle, setDefaultTitleDisplay] = useState(true);
+  const [showUserStats, setShowUserStats] = useState(false);
+  const [userPosition, setUserPosition] = useState({
+    x: undefined,
+    y: undefined,
+    time: undefined,
+  });
+  const [dataToDisplay, setDataToDisplay] = useState({});
+  const [rawData, setRawData] = useState([]);
 
-  async componentDidMount() {
-    const resp = await fetch('/trajectoires.json');
-    const data = await resp.json();
+  useEffect(() => {
+    async function fetchData() {
+      const resp = await fetch('trajectoires.json');
+      const data = await resp.json();
 
-    const sortedByTimeData = data.reduce((acc, curr) => {
+      setRawData(data);
+    }
+
+    try {
+      fetchData();
+    } catch (error) {
+      console.log('Error fetching data: ', error);
+    }
+  }, []);
+
+  useEffect(() => {
+    const sortedByTimeData = rawData.reduce((acc, curr) => {
       let client = {
         id: curr.id,
         points: curr.points.sort((a, b) => a.time - b.time),
       };
       return [...acc, client];
     }, []);
+
     const usefullData = sortedByTimeData.reduce((acc, curr) => {
       const sanitizedPoints = curr.points.reduce((acc, curr) => {
         return [...acc, { x: curr.x, y: curr.y, time: curr.time }];
@@ -40,97 +54,75 @@ class App extends React.PureComponent {
       return [...acc, sanitizedPoints];
     }, []);
 
-    this.setState({
-      dataToDisplay: usefullData,
-      rawData: data,
-      userId: data[0].id,
-    });
-  }
+    if (!userId && rawData.length !== 0) {
+      setUserId(rawData[0].id);
+    }
 
-  onHoverOverUser = (userPosition = {}, index) => {
-    const hoveredOverUserData = fromIndexToUserId(index, this.state.rawData);
-    this.setState({
-      userPosition,
-      userId: hoveredOverUserData,
-      displayDefaultTitle: false,
-    });
+    if (rawData.length !== 0) {
+      setDataToDisplay(usefullData);
+    }
+  }, [rawData, userId]);
+
+  const onHoverOverUser = (userPosition = {}, index) => {
+    const hoveredOverUserData = fromIndexToUserId(index, rawData);
+    setUserPosition(userPosition);
+    setUserId(hoveredOverUserData);
+    setDefaultTitleDisplay(false);
   };
 
-  onUserClick = (id = 0) => {
-    const { rawData = [] } = this.state;
+  const onUserClick = (id = 0) => {
     const index = rawData.findIndex(u => u.id === id);
-    this.setState({
-      index,
-      userId: id,
-      displayDefaultTitle: true,
-      showUserStats: true,
-    });
+    setIndex(index);
+    setUserId(id);
+    setDefaultTitleDisplay(true);
+    setShowUserStats(true);
   };
 
-  onButtonClick = () => {
-    this.setState(prevState => ({
-      showAll: !prevState.showAll,
-      displayDefaultTitle: !prevState.displayDefaultTitle,
-      showUserStats: !prevState.showUserStats,
-    }));
+  const onButtonClick = () => {
+    setShowAll(!showAll);
+    setDefaultTitleDisplay(!displayDefaultTitle);
+    setShowUserStats(!showUserStats);
   };
 
-  render() {
-    const {
-      dataToDisplay = [],
-      rawData = [],
-      index,
-      userId,
-      userPosition: { x, y, time } = {},
-      showAll,
-      displayDefaultTitle,
-      showUserStats,
-    } = this.state;
-    return (
-      <div className="App">
-        <header className="App-header">
-          <PositionInfo
-            displayDefaultTitle={displayDefaultTitle}
-            userId={userId}
-            time={time}
-            x={x}
-            y={y}
-          />
-          <div className="container">
-            <div className="customers">
-              <button
-                id="button"
-                onClick={this.onButtonClick}
-              >
-                Show All Customers
-              </button>
-              {rawData.map((user, i) => {
-                return (
-                  <User onUserClick={this.onUserClick} key={i} id={user.id} />
-                );
-              })}
-            </div>
-            <div className="graph">
-              <Graph
-                onHoverOverUser={this.onHoverOverUser}
-                dataToDisplay={dataToDisplay}
-                rawData={rawData}
-                index={index}
-                userId={userId}
-                showAll={showAll}
-              />
-              <UserDetail
-                userId={userId}
-                index={index}
-                data={dataToDisplay}
-                showUserStats={showUserStats}
-              />
-            </div>
+  return (
+    <div className="App">
+      <header className="App-header">
+        <PositionInfo
+          displayDefaultTitle={displayDefaultTitle}
+          userId={userId}
+          time={userPosition.time}
+          x={userPosition.x}
+          y={userPosition.y}
+        />
+        <div className="container">
+          <div className="customers">
+            <button id="button" onClick={onButtonClick}>
+              Show All Customers
+            </button>
+            {rawData.map((user, i) => {
+              return <User onUserClick={onUserClick} key={i} id={user.id} />;
+            })}
           </div>
-        </header>
-      </div>
-    );
-  }
+          <div className="graph">
+            <Graph
+              onHoverOverUser={onHoverOverUser}
+              dataToDisplay={dataToDisplay}
+              rawData={rawData}
+              index={index}
+              userId={userId}
+              showAll={showAll}
+            />
+            <UserDetail
+              userId={userId}
+              index={index}
+              data={dataToDisplay}
+              showUserStats={showUserStats}
+            />
+          </div>
+        </div>
+      </header>
+    </div>
+  );
 }
 
 export default App;
