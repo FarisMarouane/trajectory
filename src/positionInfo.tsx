4a0e5464ff02fc2e3a22f495cc3071f0cb5c0088
Marouane Faris <faris.marouane@gmail.com>
2019-09-22 16:41:22 +0200

    Migrate positionInfo.tsx

--- src/positionInfo.tsx
+++ src/positionInfo.tsx
@@ -1,6 +1,18 @@
 import React from 'react';
 
-const PositionInfo = ({ x, y, time, userId, displayDefaultTitle }) => {
+const PositionInfo = ({
+  x,
+  y,
+  time,
+  userId,
+  displayDefaultTitle,
+}: {
+  x: number;
+  y: number;
+  time: number;
+  userId: string;
+  displayDefaultTitle: boolean;
+}) => {
   if (displayDefaultTitle || x === undefined || y === undefined)
     return <h2 className="title">Hover over a position</h2>;
   return (
