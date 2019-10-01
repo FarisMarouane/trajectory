64d62dfa3219ec251adae812d85e6482a6129505
Marouane Faris <faris.marouane@gmail.com>
2019-10-01 13:52:55 +0200

    Migrate userDetail.tsx

--- src/userDetail.tsx
+++ src/userDetail.tsx
@@ -1,38 +1,49 @@
 import React from 'react';
+import { positionInTime } from './App';
 
 import { calculateTotalTime, calculateAverageSpeed } from './helpers';
 
-const UserDetail = ({ showUserStats, userId, index, data }) => {
+const UserDetail = ({
+  showUserStats,
+  userId,
+  index,
+  data,
+}: {
+  showUserStats: boolean;
+  userId: string;
+  index: number;
+  data: Array<Array<positionInTime>>;
+}) => {
   if (!showUserStats) return null;
   return (
     <table id="table">
       <thead>
         <tr>
-          <th colSpan="4">{`Customer ${userId} stats`}</th>
+          <th colSpan={4}>{`Customer ${userId} stats`}</th>
         </tr>
       </thead>
       <tbody>
         <tr>
-          <td className="table-cell" width="30%">
+          <td className="table-cell" style={{ width: '30%' }}>
             Total Time
           </td>
-          <td className="table-cell" width="70%">
+          <td className="table-cell" style={{ width: '70%' }}>
             {calculateTotalTime(data, index)}
           </td>
         </tr>
         <tr>
-          <td className="table-cell" width="30%">
+          <td className="table-cell" style={{ width: '30%' }}>
             Number of stops
           </td>
-          <td className="table-cell" width="70%">
+          <td className="table-cell" style={{ width: '70%' }}>
             {data[index].length}
           </td>
         </tr>
         <tr>
-          <td className="table-cell" width="30%">
+          <td className="table-cell" style={{ width: '30%' }}>
             Average speed
           </td>
-          <td className="table-cell" width="70%">
+          <td className="table-cell" style={{ width: '70%' }}>
             {calculateAverageSpeed(data, index)}
           </td>
         </tr>
