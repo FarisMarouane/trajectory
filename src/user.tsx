49c3f315428aead84d0db003a423bd8998c9765a
Marouane Faris <faris.marouane@gmail.com>
2019-10-01 13:53:54 +0200

    Fix typescript type typo in user.tsx

--- src/user.tsx
+++ src/user.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const User = ({ id, onUserClick }: { id: String, onUserClick: Function }) => {
+const User = ({ id, onUserClick }: { id: string, onUserClick: Function }) => {
   return (
     <div id="user" onClick={() => onUserClick(id)}>
       {id}
